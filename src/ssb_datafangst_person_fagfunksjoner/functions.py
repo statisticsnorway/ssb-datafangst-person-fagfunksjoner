4461158d58d35fff6c3ef3286baf3c2e4ab5077e
Elise <Alstad>
2024-11-05 13:29:10 +0100

    lagt til eksempel tester

--- src/ssb_datafangst_person_fagfunksjoner/functions.py
+++ src/ssb_datafangst_person_fagfunksjoner/functions.py
@@ -1,3 +1,24 @@
+def example_function(number1: int, number2: int) -> str:
+    """Compare two integers.
+    This is merely an example function can be deleted. It is used to show and test generating
+    documentation from code, type hinting, testing, and testing examples
+    in the code.
+    Args:
+        number1: The first number.
+        number2: The second number, which will be compared to number1.
+    Returns:
+        A string describing which number is the greatest.
+    Examples:
+        Examples should be written in doctest format, and should illustrate how
+        to use the function.
+        >>> example_function(1, 2)
+        1 is less than 2
+    """
+    if number1 < number2:
+        return f"{number1} is less than {number2}"
+    return f"{number1} is greater than or equal to {number2}"
+
+
 import pandas as pd
 from dapla import FileClient
 import pyarrow.parquet as pq
