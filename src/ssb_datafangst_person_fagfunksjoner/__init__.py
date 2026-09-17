78a00bc95fbf3897a2fdaea59003c17a5b1cbdf5
AnneAbelseth <aae@ssb.no>
2026-09-17 14:01:01 +0200

    huska __init__.py også..

--- src/ssb_datafangst_person_fagfunksjoner/__init__.py
+++ src/ssb_datafangst_person_fagfunksjoner/__init__.py
@@ -1,3 +1,4 @@
 """SSB Datafangst Person Fagfunksjoner."""
 from .velg_skjema import velg_skjema
 from .functions import *
+from .klass_utils import *
--- src/ssb_datafangst_person_fagfunksjoner/velg_skjema.py
+++ src/ssb_datafangst_person_fagfunksjoner/velg_skjema.py
@@ -11,42 +11,6 @@ from IPython.display import display, clear_output, HTML
 import ipywidgets as widgets    
 
 
-# +
-# def skjema_info(): 
-#     """
-#     Oppdaterer og lagrer variabler med info om undersøkelsen etter bruker har valgt undersøkelse i dropdown. Valget blir også lagret så dette slipper å bli gjort i hver notebook. 
-#     """
-#     global InstrumentId 
-#     global skjemanavn 
-#     b =' \033[1m'
-#     bb = '\033[0m'
-    
-#     skjemanavn =  dropdown_widget.value
-#     if skjemanavn != '': 
-#         filepath = "gs://ssb-datafangst-person-data-produkt-prod/skjemadatabase/*.parquet"
-#         fs = FileClient.get_gcs_file_system()
-
-#         files = fs.glob(filepath) 
-#         df = (
-#             pq.ParquetDataset(files, 
-#                         filesystem=fs, 
-#                        )
-#         ).read().to_pandas() 
-
-#         df = df.dropna(subset=['InstrumentId'])
-
-#         skjemanavn =  dropdown_widget.value
-#         InstrumentId = df.loc[df['Skjemanavn'] == dropdown_widget.value, 'InstrumentId'].values[0] 
-#         %store InstrumentId
-#         %store skjemanavn 
-#         print(f"\nDu har valgt å se data om {b}{skjemanavn}{bb}, InstrumentId er {InstrumentId}")
-
-#         return InstrumentId, skjemanavn
-#     else: 
-#         print('velg et skjema i dropdown listen')    
-# # funksjon for å oppdatere info her
-# -
-
 def velg_skjema():
     from pathlib import Path
     import pyarrow.parquet as pq
@@ -141,6 +105,42 @@ def velg_skjema():
     )
 
     return valg
+
+# +
+# def skjema_info(): 
+#     """
+#     Oppdaterer og lagrer variabler med info om undersøkelsen etter bruker har valgt undersøkelse i dropdown. Valget blir også lagret så dette slipper å bli gjort i hver notebook. 
+#     """
+#     global InstrumentId 
+#     global skjemanavn 
+#     b =' \033[1m'
+#     bb = '\033[0m'
+    
+#     skjemanavn =  dropdown_widget.value
+#     if skjemanavn != '': 
+#         filepath = "gs://ssb-datafangst-person-data-produkt-prod/skjemadatabase/*.parquet"
+#         fs = FileClient.get_gcs_file_system()
+
+#         files = fs.glob(filepath) 
+#         df = (
+#             pq.ParquetDataset(files, 
+#                         filesystem=fs, 
+#                        )
+#         ).read().to_pandas() 
+
+#         df = df.dropna(subset=['InstrumentId'])
+
+#         skjemanavn =  dropdown_widget.value
+#         InstrumentId = df.loc[df['Skjemanavn'] == dropdown_widget.value, 'InstrumentId'].values[0] 
+#         %store InstrumentId
+#         %store skjemanavn 
+#         print(f"\nDu har valgt å se data om {b}{skjemanavn}{bb}, InstrumentId er {InstrumentId}")
+
+#         return InstrumentId, skjemanavn
+#     else: 
+#         print('velg et skjema i dropdown listen')    
+# # funksjon for å oppdatere info her
+# -
 
 
 
